e0be981bf3691ede73855d01bab34d9eec9ad3d6
MonsterGene <monster.j.zhang@mail.foxconn.com>
2022-01-24 11:17:16 +0800

    update

--- utils/constant.js
+++ utils/constant.js
@@ -1,6 +1,12 @@
-const UPLOAD_PATH = process.env.NODE_ENV === 'development' ? 'D:\\StudySpace\\NginxWWW\\imooc-ebook-admin\\upload' : '/root/nginx/www/ebook-admin-upload'
+const isDev = process.env.NODE_ENV === 'development'
+
+const UPLOAD_PATH = isDev
+  ? 'D:\\StudySpace\\NginxWWW\\imooc-ebook-admin\\upload'
+  : '/root/nginx/www/ebook-admin-upload'
 const OLD_UPLOAD_URL = 'http://ebook.ourclubs.cn:8189/book/res/img'
-const UPLOAD_URL = 'http://ebook.ourclubs.cn:8189'
+const UPLOAD_URL = isDev
+  ? 'http://ebook.ourclubs.cn:8189'
+  : 'https://ebook.ourclubs.cn/res/ebook-admin-upload'
 
 module.exports = {
   CODE_TOKEN_EXPIRED: -2,
